fa63c0824a69ff0180afca3925e551f4d1ee1053
Tomas Kender <tomaskender1@gmail.com>
2019-12-15 04:00:38 +0100

    uloha 6

--- src/4.py
+++ src/4.py
@@ -1,19 +1,34 @@
 from scipy.io import wavfile
 from scipy.signal import spectrogram, lfilter, freqz, tf2zpk
 from scipy.stats import pearsonr
+from os import listdir
 import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sf
 import IPython
 import math
 
+
 obj = []
 
-def calculate_features(filename, invert_axis):
+def calculate_features(filename, is_query):
     global obj
 
-    s, fs = sf.read(filename)
+    folder = ''
+    if is_query:
+        folder = './queries/'
+    else:
+        folder = './sentences/'
+
+    s, fs = sf.read(folder + filename)
     t = np.arange(s.size) / fs
+
+    plt.figure(figsize=(9,3))
+    plt.plot(t, s)
+    #plt.gca().set_title('Audio signal of ' + filename)
+    plt.gca().set_xlabel('t')
+    plt.margins(x=0)
+    plt.savefig('signal_' + filename + '.pdf')
 
     f, t, sgr = spectrogram(s, fs,nperseg=400, noverlap=240, nfft= 511)
     sfgr_log = 10 * np.log10(sgr+1e-20) 
@@ -31,16 +46,13 @@ def calculate_features(filename, invert_axis):
 
     plt.figure(figsize=(9,3))
     plt.pcolormesh(t, range(int(obj.size/obj[0].size)), obj)
-    plt.gca().set_title(filename)
-    plt.gca().set_xlabel('Čas [s]')
-    plt.gca().set_ylabel('Frekvencia [Hz]')
-    cbar = plt.colorbar()
-    cbar.set_label('Spektrálna hustota výkonu [dB]', rotation=270, labelpad=15)
+    #plt.gca().set_title(filename)
+    plt.gca().set_xlabel('t')
+    plt.gca().set_ylabel('Features')
 
-    if invert_axis:
-        plt.gca().invert_yaxis()
+    plt.gca().invert_yaxis()
     plt.tight_layout()
-    return plt
+    plt.savefig('spectro_' + filename + '.pdf')
 
 def calculate_score(sentence_matica, sentence_pp, query_matica):
     sentence_matica = np.array(sentence_matica)
@@ -51,24 +63,35 @@ def calculate_score(sentence_matica, sentence_pp, query_matica):
     return curr_score/len(query_matica[0])
 
 
-def create_similarity_graph(sentence_matica, query_matica):
+def calculate_score_mat(sentence_matica, query_matica):
     score_mat = []
     for pp in range(len(sentence_matica[0])-len(query_matica[0])):
         score_mat.append(calculate_score(sentence_matica, pp, query_matica))
+    return score_mat
+    
+def create_similarity_graph(filename):
+    calculate_features(filename, False)
+    sentence_matica = obj
+    
+    calculate_features('q1.wav', True)
+    q1_score = calculate_score_mat(sentence_matica, obj)
+    calculate_features('q2.wav', True)
+    q2_score = calculate_score_mat(sentence_matica, obj)
+
     plt.figure(figsize=(9,3))
     plt.ylim(0,1)
-    plt.plot(np.arange(len(score_mat))/100, score_mat)
-    plt.gca().set_title('Priebeh skóre')
-    plt.gca().set_xlabel('Čas [s]')
-    plt.gca().set_ylabel('Skóre')
-
+    plt.margins(x=0)
+    plt.plot(np.arange(len(q1_score))/100, q1_score, label='effective')
+    plt.plot(np.arange(len(q2_score))/100, q2_score, label='appreciated')
+    plt.legend(loc='upper right')
+    #plt.gca().set_title('effective and appreciated vs ' + filename)
+    plt.gca().set_xlabel('t')
+    plt.gca().set_ylabel('Scores')
     plt.tight_layout()
-    plt.savefig("skore.pdf")
-    
+    plt.savefig('score_' + filename + '.pdf')
 
-calculate_features('./sentences/si512.wav', False).savefig('si512.pdf')
-sent_m = obj
-calculate_features('./queries/q1.wav', True).savefig('q1.pdf')
-query_m = obj
+def create_score_graphs():
+    for file in listdir('./sentences/'):
+        create_similarity_graph(file)
 
-create_similarity_graph(sent_m, query_m)
+create_score_graphs()
